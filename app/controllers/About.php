20046f5173263da9972ccfe9a1f26d6a2515675f
muhmp <defcoolz@gmail.com>
2023-01-12 23:20:23 +0900

    views updates

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,15 +1,23 @@
 <?php
 
-require_once 'core/Controller.php';
 
 //class
 class About extends Controller
 {
 
-    //method index as default 
+    //method index
     public function index($name = 'Mamet', $job = 'Software engineering', $age = 27)
     {
-        $this->view('about/index');
+        $data['name'] = $name;
+        $job = urldecode($job);
+        $data['job'] = $job;
+        $data['age'] = $age;
+
+
+
+        //call the about/index folder
+
+        $this->view('about/index', $data);
     }
     //method
     public function page()
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -10,6 +10,7 @@
 
 <body>
     <h1>About Me</h1>
+    <p> My name is <?= $data['name']; ?>, I'm a <?= $data['job']; ?>. I am <?= $data['age']; ?> years old </p>
 </body>
 
 </html>
