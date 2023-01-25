cf4d20d1a97648813788c1d188c1e27fe5703ce5
muhmp <defcoolz@gmail.com>
2023-01-25 19:27:12 +0900

    Assets implementation
    assets implementation using bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p> My name is <?= $data['name']; ?>, I'm a <?= $data['job']; ?>. I am <?= $data['age']; ?> years old </p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/dalle.png" alt="dalle pic" width="200" class="rounded-circle mt-4">
+    <p class="mt-4"> My name is <?= $data['name']; ?>, I'm a <?= $data['job']; ?>. I am <?= $data['age']; ?> years old </p>
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,22 +8,17 @@
 </head>
 
 <body>
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">MVC PHP</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <!--navbar  -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
+        <div class="container">
+            <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="<?= BASEURL; ?>">About</a>
+                </div>
+            </div>
         </div>
     </nav>
